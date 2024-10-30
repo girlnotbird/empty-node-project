3ff25a184f3eb86db80d24e655dc3c8b9ab1641c
Jessie McCollum <mccoljessie@gmail.com>
2024-10-29 23:03:27 -0400

    Update: statically serve src/public/app

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
+import { __dirname } from "./utilities/node.js";
 import express from "express";
 
+const DIRNAME = __dirname(import.meta);
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 const HOSTNAME = process.env.HOSTNAME ? process.env.HOSTNAME : "0.0.0.0";
 const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : "production";
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send("Hello, World.").end();
-});
+server.use(express.static(DIRNAME + '/public/app'));
 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Now listening on port ${String(PORT)}...`);
